ef7fa788c2e35b80471ce2028b1773d8152e43dd
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-11-19 06:00:40 +0900

    ImageGalleryライブラリの実行ok

--- pages/bucket.jsx
+++ pages/bucket.jsx
@@ -1,3 +1,43 @@
+import React from "react";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+
 export default function Bucket() {
-  return <h1>My Bucket-List</h1>;
+  const style = {
+    width: "50%",
+    margin: "0 auto",
+    marginTop: 150,
+  };
+
+  return (
+    <div className="App">
+      <div style={style}>
+        <Sample />
+      </div>
+    </div>
+  );
 }
+
+const Sample = () => {
+  const images = [
+    {
+      original:
+        "https://cdn.pixabay.com/photo/2020/03/09/23/04/plum-4917370_960_720.jpg",
+      thumbnail:
+        "https://cdn.pixabay.com/photo/2020/03/09/23/04/plum-4917370_960_720.jpg",
+    },
+    {
+      original:
+        "https://cdn.pixabay.com/photo/2020/02/21/18/43/kosmeen-4868375_960_720.jpg",
+      thumbnail:
+        "https://cdn.pixabay.com/photo/2020/02/21/18/43/kosmeen-4868375_960_720.jpg",
+    },
+    {
+      original:
+        "https://cdn.pixabay.com/photo/2016/04/16/12/50/chrysanthemum-1332994_960_720.jpg",
+      thumbnail:
+        "https://cdn.pixabay.com/photo/2016/04/16/12/50/chrysanthemum-1332994_960_720.jpg",
+    },
+  ];
+  return <ImageGallery items={images} />;
+};
