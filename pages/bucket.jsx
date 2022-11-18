1ab1e0106b8afbd6df80769c633272e4e8e45641
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-11-19 06:18:24 +0900

    LoaderSpinnerの動作確認◎

--- pages/bucket.jsx
+++ pages/bucket.jsx
@@ -1,3 +1,30 @@
+import { CirclesWithBar, ColorRing } from "react-loader-spinner";
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 export default function Bucket() {
-  return <h1>My Bucket-List</h1>;
+  return (
+    <>
+      <CirclesWithBar
+        height="100"
+        width="100"
+        color="#4fa94d"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        outerCircleColor=""
+        innerCircleColor=""
+        barColor=""
+        ariaLabel="circles-with-bar-loading"
+      />
+      <ColorRing
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+      />
+    </>
+  );
 }
